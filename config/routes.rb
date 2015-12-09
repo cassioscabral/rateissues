c850215779cf61f7244ea305438d6ca0b0bee25f
Cassio Cabral <cassioscabral@gmail.com>
2015-12-09 20:36:26 -0300

    add route to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :user, only: []
+  resource :login, only: [:create], controller: :sessions
 end
